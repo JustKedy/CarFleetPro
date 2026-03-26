60a63661524047bea297f8b1af919d75ba63c0ec
Alper Topal <alper.topal.2006@gmail.com>
2026-03-26 20:57:32 +0300

    Added a few Data and Models

---
+++ CarFleetPro.API/Models/Enums.cs
@@ -0,0 +1,17 @@
+﻿namespace CarFleetPro.API.Models
+{
+   
+    public enum VehicleType { Sedan, SUV, Hatchback, Van }
+    public enum FuelType { Benzin, Dizel, Elektrik }
+    public enum TransmissionType { Manuel, Otomatik }
+    public enum VehicleStatus { Available, Rented, Maintenance }
+
+    public enum RentalStatus { Active, Completed, Cancelled }
+    public enum PaymentMethod { Nakit, KrediKarti, Havale }
+    public enum MaintenanceStatus { Planned, InProgress, Done }
+
+    public class Enums
+    {
+
+    }
+}
---
+++ CarFleetPro.API/Models/Payment.cs
@@ -0,0 +1,15 @@
+﻿namespace CarFleetPro.API.Models
+{
+    public class Payment
+    {
+        public int PaymentId { get; set; }
+
+        public int RentalId { get; set; }
+        public Rental? Rental { get; set; }
+
+        public decimal Amount { get; set; }
+        public PaymentMethod PaymentMethod { get; set; }
+        public DateTime PaymentDate { get; set; }
+        public bool IsPaid { get; set; } = false;
+    }
+}
