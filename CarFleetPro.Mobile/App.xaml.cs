6456ac33ede2b0064ca7ea271e5e7d88330b262f
Kadir <atoksoz67@gmail.com>
2026-04-04 21:21:53 +0300

    yama

--- CarFleetPro.Mobile/App.xaml.cs
+++ CarFleetPro.Mobile/App.xaml.cs
@@ -1,17 +1,23 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 
-namespace CarFleetPro.Mobile
-{
-    public partial class App : Application
-    {
-        public App()
-        {
-            InitializeComponent();
-        }
+namespace CarFleetPro.Mobile;
 
-        protected override Window CreateWindow(IActivationState? activationState)
-        {
-            return new Window(new AppShell());
-        }
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+    }
+
+    protected override Window CreateWindow(IActivationState? activationState)
+    {
+        // MAUI'nin varsayılan penceresini oluşturuyoruz (AppShell'i çağırarak)
+        var window = new Window(new AppShell());
+
+        // Pencere boyutlarını telefon formatına (dikey) sabitliyoruz
+        window.Width = 400;
+        window.Height = 850;
+
+        return window;
     }
 }
